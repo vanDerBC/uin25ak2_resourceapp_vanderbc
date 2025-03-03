534f531c98078d4af30a3b243d84f35252b8ca19
vanDerBC <tobiasbc@hiof.no>
2025-03-03 12:05:47 +0100

    Uppdate
    lagt til route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
 import { useState } from 'react'
+
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Layout from "./components/Layout"
+import { Routes, Route } from 'react-router-dom'
+import Resources from './components/Resources'
+
+function App() {
+
+  return (
+    <Layout>
+      <Routes>
+        <Route path='/html' element={<Resources category={"HTML"} />}></Route>
+        <Route path='/css' element={<Resources category={"CSS"} />}></Route>
+        <Route path='/javascript' element={<Resources category={"JavaScript"} />}></Route>
+        <Route path='react' element={<Resources category={"React"} />}></Route>
+        <Route path='/sanityandheadlesscms' element={<Resources category={"Sanity and headless CMS"} />}></Route>
+      </Routes>
+    </Layout>
+  )
+}
+
+export default App
+
+
+/**
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -33,3 +62,4 @@ function App() {
 }
 
 export default App
+*/
--- src/assets/javascript/ressurser.js
+++ src/assets/javascript/ressurser.js
@@ -94,3 +94,5 @@ const resources = [
         ]
     },
 ]
+
+export default resources;
